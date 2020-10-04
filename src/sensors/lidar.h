ca83362e1d75daa0081b6e48e299825ecb90ab39
PyaeSoanAung <server02.psa1981@gmail.com>
2020-10-04 19:40:47 +0630

    v2

--- src/sensors/lidar.h
+++ src/sensors/lidar.h
@@ -95,12 +95,12 @@ struct Lidar
 		groundSlope = setGroundSlope;
 
 		// TODO:: increase number of layers to 8 to get higher resoultion pcd
-		int numLayers = 3;
+		int numLayers = 8;
 		// the steepest vertical angle
 		double steepestAngle =  30.0*(-pi/180);
 		double angleRange = 26.0*(pi/180);
 		// TODO:: set to pi/64 to get higher resoultion pcd
-		double horizontalAngleInc = pi/6;
+		double horizontalAngleInc = pi/64;
 
 		double angleIncrement = angleRange/numLayers;
 
